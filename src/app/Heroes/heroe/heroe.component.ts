db06dd4a5ecfa4f1d63b7d0094a81a6c41f9800a
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-02 02:22:07 -0300

    Crear componente de forma automática

--- src/app/Heroes/heroe/heroe.component.ts
+++ src/app/Heroes/heroe/heroe.component.ts
@@ -1,10 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-heroe',
-    templateUrl: 'heroe.component.html'
-
+  selector: 'app-heroe',
+  templateUrl: 'heroe.component.html',
+  styles: [''],
 })
-export class HeroeComponent{
-
+export class HeroeComponent {
+  nombre: string = 'Ironman';
+  edad: number = 45;
+  get nombreCapitalizado(): string {
+    return this.nombre.toUpperCase();
+  }
+  obtenerNombre(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+  cambiarNombre(): void {
+    this.nombre = 'Spiderman';
+  }
+  cambiarEdad(): void {
+    this.edad = 30;
+  }
 }
