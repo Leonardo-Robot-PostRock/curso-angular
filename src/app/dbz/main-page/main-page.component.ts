05c2d65f1452dbf24e5d9fdf9c874fe16f5906b9
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-04 14:55:56 -0300

    Metodo para cambiar nombre del boton de formulario

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000,
+  };
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+  }
+
   agregar() {
-    console.log('Esto es una prueba');
+    console.log(this.nuevo);
+    return this.nuevo;
   }
 }
