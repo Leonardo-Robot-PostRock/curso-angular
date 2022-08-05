ba77bba8dd53119cc5f72f21dd21bb64f16855a7
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-05 19:02:18 -0300

    Se agregan componentes a la declaracion en module

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarPersonajeComponent } from './agregar-personaje/agregar-personaje.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    PersonajesComponent
+    PersonajesComponent,
+    AgregarPersonajeComponent
   ],
   exports: [
     MainPageComponent
