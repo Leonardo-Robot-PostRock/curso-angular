c6f231a23450e7e14c297ba26585c68ef33546ed
Leonardo Puebla <leonardo38361@gmail.com>
2022-08-05 19:03:47 -0300

    Se quita la variable y método

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,36 +1,17 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
 
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [];
   nuevo: Personaje = {
     nombre: '',
     poder: 0,
   };
-
-  personajes: Personaje[] = [];
-
   // cambiarNombre(event: any) {
   //   console.log(event.target.value);
   // }
-
-  agregar(personaje: any) {
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0,
-    };
-    console.log('ver', personaje);
-
-    return this.nuevo;
-  }
 }
