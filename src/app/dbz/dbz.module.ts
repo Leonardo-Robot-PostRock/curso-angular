a5a7aa0d1e9c0c92454ee072dc12c2656364f6f7
Leonardo Puebla <leonardo38361@gmail.com>
2022-10-18 22:39:23 -0300

    se exporta servicio dbz

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -5,19 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarPersonajeComponent } from './agregar-personaje/agregar-personaje.component';
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     PersonajesComponent,
-    AgregarPersonajeComponent
+    AgregarPersonajeComponent,
   ],
-  exports: [
-    MainPageComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ]
+  exports: [MainPageComponent],
+  imports: [CommonModule, FormsModule],
+  providers: [DbzService],
 })
-export class DbzModule { }
+export class DbzModule {}
