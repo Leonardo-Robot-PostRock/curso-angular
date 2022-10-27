0d68cabeb7a1b88656d49348ee21b2907c6f5163
Leonardo Puebla <leonardo38361@gmail.com>
2022-10-27 15:57:35 -0300

    Se injecta el servicio dbzService}

--- src/app/dbz/agregar-personaje/agregar-personaje.component.ts
+++ src/app/dbz/agregar-personaje/agregar-personaje.component.ts
@@ -1,20 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar-personaje',
   templateUrl: './agregar-personaje.component.html',
 })
 export class AgregarPersonajeComponent {
-  constructor() {}
+  constructor(private dbzService: DbzService) {}
 
   @Input() nuevo: Personaje = {
     nombre: '',
     poder: 0,
   };
 
-  @Output() onNuevoPersonaje: EventEmitter<Personaje> =
-    new EventEmitter<Personaje>();
+  // @Output() onNuevoPersonaje: EventEmitter<Personaje> =
+  //   new EventEmitter<Personaje>();
 
   // cambiarNombre(event: any) {
   //   console.log(event.target.value);
@@ -25,13 +26,11 @@ export class AgregarPersonajeComponent {
       return;
     }
 
-    console.log('Nuevo', this.nuevo);
-    this.onNuevoPersonaje.emit(this.nuevo);
+    // this.onNuevoPersonaje.emit(this.nuevo);
+    this.dbzService.agregarPersonaje(this.nuevo);
     this.nuevo = {
       nombre: '',
       poder: 0,
     };
-
-    return this.nuevo;
   }
 }
